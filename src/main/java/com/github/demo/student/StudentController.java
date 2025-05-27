3f848ac1ffe22fa00cb92afab862424b670099b6
akhterjaved <akhterjaved166@gmail.com>
2025-05-28 00:03:06 +0530

    building the first sample API

---
+++ src/main/java/com/github/demo/student/StudentController.java
@@ -0,0 +1,23 @@
+package com.github.demo.student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents() {
+        return List.of(
+                "Javed Akhtar",
+                "Monis Ali",
+                "Mohammad Kashif",
+                "Hello World"
+        );
+    }
+
+}
